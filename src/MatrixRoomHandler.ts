b4d093e2f4ae8d7a8cb9d976874e2bd4b8c9b3cb
Marco Cirillo <maranda@lightwitch.org>
2021-12-02 09:59:49 +0100

    Fix comparison

--- src/MatrixRoomHandler.ts
+++ src/MatrixRoomHandler.ts
@@ -515,7 +515,7 @@ export class MatrixRoomHandler {
             } else if (data.kicker) {
                 await intent.kick(roomId, senderMatrixUser.getId(), data.reason || undefined);
             } else {
-                if (this.config.tuning.limitStateChanges && (!data.gatewayAlias && !data.banner && !data.kicker && data.technical)) {
+                if (this.config.tuning.limitStateChanges && (!data.gatewayAlias && !data.banner && !data.kicker && !data.technical)) {
                     return;
                 }
                 await intent.leave(roomId, data.reason || undefined);
