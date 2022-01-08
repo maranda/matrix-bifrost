92164e699daa8ba6a60daa6b4b4b744be0059404
Marco Cirillo <maranda@lightwitch.org>
2022-01-08 10:52:30 +0100

    Reintroduce partail deduplication for presences

--- src/xmppjs/XJSInstance.ts
+++ src/xmppjs/XJSInstance.ts
@@ -527,9 +527,10 @@ export class XmppJsInstance extends EventEmitter implements IBifrostInstance {
     private async onStanza(stanza: Element) {
         const startedAt = Date.now();
         const id = stanza.attrs.id = stanza.attrs.id || this.generateIdforMsg(stanza);
-        if (this.seenMessages.has(id) && stanza.name === "message") {
+        if (this.seenMessages.has(id) && (stanza.name === "message" ||
+            (stanza.name === "presence" && stanza.attrs.type !== "unavailable"))) {
             return;
-        } else if (stanza.name === "message") {
+        } else if (stanza.name === "message" || stanza.name === "presence") {
             this.xmppAddSentMessage(id);
         }
         log.debug("Stanza:", stanza.toJSON());
