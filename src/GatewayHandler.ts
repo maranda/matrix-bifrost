695f31afddd42412edf0c08f7a4b137c8597388c
Marco Cirillo <maranda@lightwitch.org>
2022-02-02 16:35:33 +0100

    Clean room from promise cache on ESOCKETTIMEDOUT

--- src/GatewayHandler.ts
+++ src/GatewayHandler.ts
@@ -257,7 +257,7 @@ export class GatewayHandler {
                 });
             }
             log.warn("Failed to join room:", ex.message);
-            if (ex.message === "Locally timed out waiting for a response") {
+            if (ex.message === "Locally timed out waiting for a response" || ex.message === "ESOCKETTIMEDOUT") {
                 // attempt cleaning stale promises from the cache caused by timeouts on bridge startup sequences
                 this.roomIdCache.delete(roomId);
             }
